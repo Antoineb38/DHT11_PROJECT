49b481cc94dd64648a0c2f855798df6aaa3c34ec
antoine <bergamoantoine@gmail.com>
2018-02-01 15:08:53 +0100

    update

---
+++ classes/measures.php
@@ -0,0 +1,12 @@
+<?php
+class Measure {
+    public $id;
+    public $datetime;
+    public $temperature;
+    public $humidite;
+    public function __construct($datetime, $temperature, $humidite) {
+        $this->datetime = $datetime;
+        $this->temperature = $temperature;
+        $this->humidite = $humidite;
+    }
+}
